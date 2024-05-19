37d334a103fcda1fdf59fe1d079c169bc3c12447
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-05-19 12:41:33 +0100

    added javascript code for toggling navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,32 @@ const preloader = document.querySelector("[data-preloader]");
 window.addEventListener("load", () => {
     preloader.classList.add("remove");
 })
+
+// add event on multiple elements
+const addEventOnElements = function (elements, eventType, callBack) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callBack);
+    }
+}
+
+// navbar toggler for mobile
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelectorAll("[data-overlay]");
+
+const toggleNav = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+// header
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", () => {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+})
+
